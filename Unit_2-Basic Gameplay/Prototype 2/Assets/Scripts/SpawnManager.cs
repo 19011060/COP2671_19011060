bb7f6206fb0bc8c5a1c765a2d36030cd687733b7
19011060 <cade.blanco@go.sfcollege.edu>
2024-09-11 02:10:12 -0400

    Improved Animal Spawn, Added Script File DetectCollisions for Object Collision/Destruction, Added Game Over Message

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 10;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Randomly generate animal index and spawn position
+
+    }
+
+    // Spawns a random animal from the animalPrefabs array at a random X position within the range provided
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
